bf3299342683bd86bf46d658fcf05eceb9b4a03e
Kevin Cano <kevincano@ciencias.unam.mx>
2024-09-18 13:55:50 -0600

    intento de seguir, problemas es calcular la z

--- Main.py
+++ Main.py
@@ -1,23 +1,56 @@
 import sympy as sp
+import numpy as np
 class Simplex:
-    def __init__(self,A,x,b,z):
-        self.A = A
+    def __init__(self,A,x,b,c):
+        self.A = np.matrix(A)
         self.x = x
         self.b = b
-        self.z = z
+        self.c = c
     def mostrar(self):
         p = ''
-        for n in range(len(self.A)):
-            for m in range(len(self.A[0])):
-                if self.A[n][m] < 0:
-                    p += f'-{self.A[n][m]}{self.x[m]} '
-                if self.A[n][m] > 0 and m != 0:
-                    p += f'+{self.A[n][m]}{self.x[m]} '
-                if self.A[n][m] > 0 and m == 0:
-                    p += f'{self.A[n][m]}{self.x[m]} '
+        x, y = self.A.shape
+        for n in range(x):
+            for m in range(y):
+                if self.A[n,m] < 0:
+                    p += f'-{self.A[n,m]}{self.x[m]} '
+                if self.A[n,m] > 0 and m != 0:
+                    p += f'+{self.A[n,m]}{self.x[m]} '
+                if self.A[n,m] > 0 and m == 0:
+                    p += f'{self.A[n,m]}{self.x[m]} '
                 else:
                     pass
             p += '\n'
+        p += str(c)
         print(p)
-a = Simplex([[1,2],[2,2]],['x1','x2'],'','')
+
+    def pivoteo(self,fila,columna):
+        x, y = self.A.shape
+        self.A[fila,:] /= self.A[fila,columna]
+        for f in range(x):
+            if f != fila:
+                self.A[f,:] -= self.A[f,columna]*self.A[fila,:]
+
+    def cocientes_minimos(self,columna):
+        # la columna sera la dada por optimalidad
+        lista = []
+        for fila in range(columna):
+            lista.append(self.A[fila,columna])
+        cociente = max(lista)
+        return lista.index(cociente)
+            
+    def optimalidad(self):
+        posicion = None
+        actual = 0
+        for x in self.A[-1, :]:
+            actual = x
+            if x > 0:
+                posicion = self.A.index(x)
+                break
+        return posicion, actual
+
+a = [[1,2],[2,2]]
+x = ['x1','x2']
+c = [-1, 2]
+a = Simplex(a,x,'',c)
 a.mostrar()
+print(a.optimalidad())
