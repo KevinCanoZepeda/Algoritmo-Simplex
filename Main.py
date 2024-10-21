aebca7159a8dea9773a93dbff70b5dbb3fd90e5b
Kevin Cano <kevincano@ciencias.unam.mx>
2024-10-20 22:48:20 -0600

    reinicio del proyecto

--- Main.py
+++ Main.py
@@ -1,56 +1,54 @@
-import sympy as sp
-import numpy as np
 class Simplex:
-    def __init__(self,A,x,b,c):
-        self.A = np.matrix(A)
-        self.x = x
+    def __init__(self,A,b,c,tipo = 'Max'):
+        self.A = A
         self.b = b
         self.c = c
+        self.modelo = None
+        self.tipo = tipo
+    def construir(self):
+        # cosntruimos una matriz de ceros
+        self.modelo = [ [0 for i in range(len(self.A[0])+1)] for i in range(len(self.b) + 1)]
+        # construccion de la A principal
+        for i in range(len(self.A)):# filas
+            for j in range(len(self.A[0])): # columnas
+                self.modelo[i][j] = self.A[i][j]
+        # construccion de la fila de c
+        for j in range(len(self.A[0])):
+            self.modelo[-1][j] = self.c[j]
+        # cosntruccion de la columna b
+        for i in range(len(self.A)):
+            self.modelo[i][-1] = self.b[i]
+        self.mostrar()
     def mostrar(self):
-        p = ''
-        x, y = self.A.shape
-        for n in range(x):
-            for m in range(y):
-                if self.A[n,m] < 0:
-                    p += f'-{self.A[n,m]}{self.x[m]} '
-                if self.A[n,m] > 0 and m != 0:
-                    p += f'+{self.A[n,m]}{self.x[m]} '
-                if self.A[n,m] > 0 and m == 0:
-                    p += f'{self.A[n,m]}{self.x[m]} '
-                else:
-                    pass
-            p += '\n'
-        p += str(c)
-        print(p)
+        for x in range(len(self.b)+1):
+            print(self.modelo[x])
+    def encontrar_pivote(self):
+        pass
+    def pivotear(self):
+    # Volvemos 1 la fila del pivote
+        if self.encontrar_pivote() != False:
+            i, j = self.encontrar_pivote()  # Encontrar el índice del pivote
 
-    def pivoteo(self,fila,columna):
-        x, y = self.A.shape
-        self.A[fila,:] /= self.A[fila,columna]
-        for f in range(x):
-            if f != fila:
-                self.A[f,:] -= self.A[f,columna]*self.A[fila,:]
+            # Dividimos toda la fila del pivote para que el elemento pivote sea 1
+            for y in range(len(self.modelo[0])):
+                self.modelo[i][y] /= self.modelo[i][j]
 
-    def cocientes_minimos(self,columna):
-        # la columna sera la dada por optimalidad
-        lista = []
-        for fila in range(columna):
-            lista.append(self.A[fila,columna])
-        cociente = max(lista)
-        return lista.index(cociente)
-            
-    def optimalidad(self):
-        posicion = None
-        actual = 0
-        for x in self.A[-1, :]:
-            actual = x
-            if x > 0:
-                posicion = self.A.index(x)
-                break
-        return posicion, actual
+            # Hacemos 0 en todas las demás filas de la columna del pivote
+            for x in range(len(self.modelo)):
+                if x != i:
+                    factor = self.modelo[x][j]  # Factor para la fila x
+                    for y in range(len(self.modelo[0])):
+                        # Restamos el factor multiplicado por la fila pivote
+                        self.modelo[x][y] -= factor * self.modelo[i][y]
 
-a = [[1,2],[2,2]]
-x = ['x1','x2']
-c = [-1, 2]
-a = Simplex(a,x,'',c)
-a.mostrar()
-print(a.optimalidad())
+
+
+
+
+prueba = Simplex([[0,3,-3],
+                  [0,1,1]], [10,-2],[3,2,7,0])
+prueba.construir()
+prueba.encontrar_pivote()
+prueba.pivotear()
+#prueba.mostrar()
+
