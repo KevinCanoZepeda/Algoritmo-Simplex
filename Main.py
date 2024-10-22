daea99834325f784fb035d414f8d619dcb744459
Kevin Cano <kevincano@ciencias.unam.mx>
2024-10-21 20:44:05 -0600

    Ya encuentra una solucion, agregar visualizacion grafica de mejor manera

--- Main.py
+++ Main.py
@@ -5,6 +5,7 @@ class Simplex:
         self.c = c
         self.modelo = None
         self.tipo = tipo
+    
     def construir(self):
         # cosntruimos una matriz de ceros
         self.modelo = [ [0 for i in range(len(self.A[0])+1)] for i in range(len(self.b) + 1)]
@@ -19,14 +20,38 @@ class Simplex:
         for i in range(len(self.A)):
             self.modelo[i][-1] = self.b[i]
         self.mostrar()
+    
     def mostrar(self):
         for x in range(len(self.b)+1):
             print(self.modelo[x])
+    
     def encontrar_pivote(self):
-        pass
+        i = None
+        j = None
+        # Recorremos las columnas de la fila de costos (última fila) para encontrar una positiva
+        for y in range(len(self.modelo[0]) - 1):
+            if self.modelo[-1][y] > 0:  # Si el costo es positivo, es una variable candidata para entrar
+                j = y  # Columna candidata
+                posibles_posiciones = []
+                valor_minimo = []
+                
+                # Recorremos las filas, excepto la última, para encontrar la fila del pivote
+                for x in range(len(self.modelo) - 1):
+                    if self.modelo[x][j] > 0:  # Si el coeficiente es positivo
+                        posibles_posiciones.append(x)  # Guardamos el índice de la fila
+                        valor_minimo.append(self.modelo[x][-1] / self.modelo[x][j])  # Cociente b_i/a_ij
+                
+                # Si encontramos posibles posiciones, seleccionamos la de menor cociente
+                if len(posibles_posiciones) > 0:
+                    i = posibles_posiciones[valor_minimo.index(min(valor_minimo))]
+                    #print(f'El pivote es {i},{j}')
+                    return i, j  # Retornamos las posiciones del pivote (i, j)
+        
+        return False  # Si no se encontró pivote, retornamos None, None
+
     def pivotear(self):
     # Volvemos 1 la fila del pivote
-        if self.encontrar_pivote() != False:
+        while self.encontrar_pivote() != False:
             i, j = self.encontrar_pivote()  # Encontrar el índice del pivote
 
             # Dividimos toda la fila del pivote para que el elemento pivote sea 1
@@ -40,13 +65,18 @@ class Simplex:
                     for y in range(len(self.modelo[0])):
                         # Restamos el factor multiplicado por la fila pivote
                         self.modelo[x][y] -= factor * self.modelo[i][y]
+            print("\n")
+            self.mostrar()
+            print("\n")
+        print(f'El valor de z = {self.modelo[-1][-1]*-1}')
 
 
 
 
 
-prueba = Simplex([[0,3,-3],
-                  [0,1,1]], [10,-2],[3,2,7,0])
+prueba = Simplex([[1,1,1,1,0,0],
+                  [0,1,1,0,1,0],
+                  [0,0,1,0,0,1]], [10,12,30],[2,3,-1,0,0,0])
 prueba.construir()
 prueba.encontrar_pivote()
 prueba.pivotear()
